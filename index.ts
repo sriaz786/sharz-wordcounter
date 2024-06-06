7b4cb6f7d6a41d946e7da1a5455b013ac5d9de84
sriazs786 <sriazs2013@gmail.com>
2024-06-05 22:09:09 -0700

    add all files

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const answers: {
+    Sentence : string } =  await inquirer.prompt ([{
+        name : "Sentence",
+        type : "input",
+        message : "Ente your Sentence to count the word :"
+    }])
+    const words = answers.Sentence.trim().split(" ")
+    console.log(words);
+    console.log("ypur Sentence word count is "+ words.length);
+
